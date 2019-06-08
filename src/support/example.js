4427994a90820ef42a3305b122f8867e840a1b72
Zoraida <zmcabrera@gmail.com>
2019-06-08 11:42:46 -0400

    added comment on example.js

--- src/support/example.js
+++ src/support/example.js
@@ -1,3 +1,5 @@
+/* Code based from https://github.com/tshelburne/csci-e39/blob/master/src/assignments/design-patterns/support/example.js) */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
