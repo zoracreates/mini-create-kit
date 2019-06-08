8e68f0940fd9b4f47e84412dcd78445fceb79565
Zoraida <zmcabrera@gmail.com>
2019-06-08 11:53:56 -0400

    Updates

---
+++ precache-manifest.ee1b5e2a329672cd5713e1c655b7a77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43e6645020a37d80aca29735d3bcb9d",
+    "url": "/mini-create-kit/index.html"
+  },
+  {
+    "revision": "2ab0c14bfaa5cb038300",
+    "url": "/mini-create-kit/static/css/main.6dbb06f5.chunk.css"
+  },
+  {
+    "revision": "bb3e63b41959ea453c0a",
+    "url": "/mini-create-kit/static/js/2.5fa11007.chunk.js"
+  },
+  {
+    "revision": "2ab0c14bfaa5cb038300",
+    "url": "/mini-create-kit/static/js/main.6b7fe366.chunk.js"
+  },
+  {
+    "revision": "eb19c109bac3fe006847",
+    "url": "/mini-create-kit/static/js/runtime~main.83c40fd3.js"
+  },
+  {
+    "revision": "7f6da3543bce0ddc433b911390de27b0",
+    "url": "/mini-create-kit/static/media/email.7f6da354.svg"
+  }
+]);
